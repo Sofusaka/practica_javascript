9c1de857b2e3b8ac8c2ec93a1f5eb755cacd079a
Sofía princess <sofusaka@gmail.com>
2024-02-14 20:42:14 -0500

    v2 studies!!

---
+++ scripts/1.js
@@ -0,0 +1,23 @@
+function f() {
+
+    document.getElementById('hora').innerHTML = 'childe targlia ajax';
+    console.log('help me')
+
+
+}
+
+function img() {
+
+    document.getElementById('vashwood').src = 'img/yo.jpg'
+    alert('bbygirl alert');
+
+
+}
+
+function nombre_f() {
+
+    let nombre = 'Sofia yasss';
+    document.getElementById("name").innerHTML = nombre;
+    console.log("se ejecuta???????");
+
+}
